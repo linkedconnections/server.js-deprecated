d59f6f900b549385176c49515dd1e199cf003ae0
Pieter Colpaert <pieter.colpaert@okfn.org>
2016-01-27 13:58:45 +0100

    Small mistakes in TripsController fixed

--- controllers/TripsController.js
+++ controllers/TripsController.js
@@ -4,29 +4,29 @@ var ConnectionsModel = require("../models/ConnectionsModel"),
 module.exports = function (request, response, next) {
   //1. Check whether this page is good: if it isn't, do a redirect
   //check if a datetime is set, otherwise, redirect towards the correct pageId of now
-  if (!request.query.date) {
-    response.redirect(302, request.locals.config.baseUri + '/trips/?date=' + encodeURIComponent((new Date()).toISOString().substr(0,10)));
+  if (!request.query.departureTime) {
+    response.redirect(302, request.locals.config.baseUri + '/trips/' + request.params.tripid + '/?departureTime=' + encodeURIComponent((new Date()).toISOString().substr(0,10)));
   } else {
-    var dateParam = new Date(request.query.date);
+    var dateParam = new Date(request.query.departureTime);
     if (!dateParam){
-      response.redirect(302, request.locals.config.baseUri + '/trips/?date=' + encodeURIComponent((new Date()).toISOString().substr(0,10)));
+      response.redirect(302, request.locals.config.baseUri + '/trips/' + request.params.tripid + '/?departureTime=' + encodeURIComponent((new Date()).toISOString().substr(0,10)));
     } else {
       //2. If it is a good page, then we can start streaming out the response and a HTTP 200 OK should be returned.
       // → We will now have to create a model for the data we want to retrieve from the db
       var connections = new ConnectionsModel(request.db);
       var view = new JSONLDView({
         "@context" : request.locals.config.baseUri + "/trips/context.json",
-//        "@id" : request.locals.page.getCurrentPage(),
+        "@id" : request.locals.config.baseUri + "/trips/" + request.params.tripid + '/?departureTime=' + request.query.departureTime,
         "@type" : "Collection",
         "search" : {
           "@type" : "IriTemplate",
-          "template" : request.locals.config.baseUri + "/trips/{?date}",
+          "template" : request.locals.config.baseUri + "/trips/{?departureTime}",
           "variableRepresentation" : "BasicRepresentation",
           "mapping" : {
             "@type" : "IriTemplateMapping",
             "variable" : "departureTime",
             "required" : true,
-            "property" : "http://semweb.mmlab.be/ns/linkedconnections#tripDateQuery"
+            "property" : "http://semweb.mmlab.be/ns/linkedconnections#departureTimeQuery"
           }
         }
       });
