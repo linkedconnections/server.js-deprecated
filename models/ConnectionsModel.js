7a1c17f874ca1bd726a821b500e886f584ddfc10
Sander <sander.ballieu@ugent.be>
2016-04-20 17:52:23 +0200

    Fix request in getPage function

--- models/ConnectionsModel.js
+++ models/ConnectionsModel.js
@@ -10,8 +10,8 @@ ConnectionsModel.prototype.get = function (connectionid, cb) {
   this._db.getConnection(connectionid);
 };
 
-ConnectionsModel.prototype.getPage = function (page, request, cb) {
-  this._db.getConnectionsPage(page, request, cb);
+ConnectionsModel.prototype.getPage = function (page, cb) {
+  this._db.getConnectionsPage(page, cb);
 };
 
 ConnectionsModel.prototype.create = function (connection, cb) {
