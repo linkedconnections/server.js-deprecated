1ef68e51492371127a749cbd6660d0bd381eac0a
Pieter Colpaert <pieter.colpaert@okfn.org>
2016-05-03 14:55:23 +0200

    Minutes can be configured now

--- middlewares/Paginator.js
+++ middlewares/Paginator.js
@@ -1,15 +1,16 @@
 var moment = require('moment-timezone');
 
 //This is a paginator for the connections stream
-module.exports = function (req, res, next) {  
-  //new page each 10 minutes - we can also get smarter when a profile is given which should be followed (TODO)
+module.exports = function (req, res, next) {
   if (!req.locals) {
     req.locals = {};
   }
+  //new page each X minutes - we can also get smarter when a profile is given which should be followed (TODO)
+  var pagesizeMinutes =  req.locals.config["pagesize-minutes"] || 10;
   req.locals.page = {};
   req.locals.page.getInterval = function () {
     var dt = moment.tz(req.query.departureTime, req.locals.config["default-timezone"] || "Europe/Brussels");
-    var end = moment(dt).add(10,'minutes');
+    var end = moment(dt).add(pagesizeMinutes,'minutes');
     return {
       start : dt.toDate(),
       end : end.toDate()
@@ -27,21 +28,21 @@ module.exports = function (req, res, next) {
       dt = moment.tz(dt, req.locals.config["default-timezone"] || "Europe/Brussels");
     }
 
-    // Round minutes down with modulus of 10
+    // Round minutes down with modulus of pagesizeMinutes
     var minutes = dt.minutes();
-    minutes %= 10;
+    minutes %= pagesizeMinutes;
     dt.subtract(minutes, 'minutes');
     return dt.format("YYYY-MM-DDTHH:mm");
   };
 
   req.locals.page.getNextPage = function () {
     var dt = moment.tz(req.query.departureTime, req.locals.config["default-timezone"] || "Europe/Brussels");
-    return self._base + "/connections/?departureTime=" + encodeURIComponent(dt.add(10, "minutes").format("YYYY-MM-DDTHH:mm"));
+    return self._base + "/connections/?departureTime=" + encodeURIComponent(dt.add(pagesizeMinutes, "minutes").format("YYYY-MM-DDTHH:mm"));
   }
   
   req.locals.page.getPreviousPage = function () {
     var dt = moment.tz(req.query.departureTime, req.locals.config["default-timezone"] || "Europe/Brussels");
-    return self._base + "/connections/?departureTime=" +  encodeURIComponent(dt.subtract(10, "minutes").format("YYYY-MM-DDTHH:mm"));
+    return self._base + "/connections/?departureTime=" +  encodeURIComponent(dt.subtract(pagesizeMinutes, "minutes").format("YYYY-MM-DDTHH:mm"));
   }
 
   req.locals.page.getCurrentPage = function () {
